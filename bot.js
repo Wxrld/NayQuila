3ec658f462feec32ca5e8d78939c2f89cfdc4a15
Wxrld <46580751+Wxrld@users.noreply.github.com>
2020-03-09 11:43:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', message => {
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/nayquila");
-      message.channel.send(`**Streaming Mode Activated [ :HoVerified: ]**`)
+      message.channel.send(`**Streaming Mode Activated [ :space_invader: ]**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
